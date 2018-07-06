9703238bcc2b0d3b2b8cb8cf6f0ac92637149403
huoxinchi <huoxinchi@outlook.com>
2018-07-06 13:13:45 +0800

    testcommit4

--- app/src/main/java/com/example/huoxinchi/test1/MainActivity.java
+++ app/src/main/java/com/example/huoxinchi/test1/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int anInt= 152 ;
+    int anInt= 155 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
